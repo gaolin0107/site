bf729c5a863f10f958866a4345f7fecf21179f34
Gaolin <120287763+gaolin0107@users.noreply.github.com>
2025-07-07 23:44:00 +0800

    Delete Website07.03 directory

--- Website07.03/script/loader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() { 
-  var loadingScreen = document.getElementById("loading-screen"); 
-  window.addEventListener("load", function() { 
-    loadingScreen.style.display = "none"; 
-  }); 
-}); 
