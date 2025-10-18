19edc0b28a786e53767872c4322f8d9fb18a6475
Gaolin <120287763+gaolin0107@users.noreply.github.com>
2025-10-19 01:42:17 +0800

    Delete script directory

--- script/profile.js
+++
@@ -1,7 +0,0 @@
-    const name = localStorage.getItem('userName');
-    const email = localStorage.getItem('userEmail');
-    const picture = localStorage.getItem('userPicture');
-
-    if (name) document.querySelector('.user-name').textContent = name;
-    if (email) document.querySelector('.user-email').textContent = email;
-    if (picture) document.querySelector('.profile-picture').src = picture;
